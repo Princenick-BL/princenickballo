9dd6c677b982145c95ccde58ed03e3079467568f
Princenick-BL <princenickballo@gmail.com>
2022-09-04 00:37:26 +0200

    WIP

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -6,7 +6,7 @@ import Domaines from '../../components/utills/domaines/domaines'
 import Popup from '../../components/utills/popup/popup'
 import Image from 'next/image';
 import ProjectCard from '../../components/ProjectCard';
-
+import { FaHome } from 'react-icons/fa';
 export default function Projets() {
 
   const [show_auth,setShowAuth]=useState(false)
@@ -44,56 +44,55 @@ export default function Projets() {
         {/* <!-- Meta Tags Generated via https://www.opengraph.xyz --> */}
 
       </Head>
-      <p>{new Date().toDateString()}</p>
 
       <div className="index LayoutVerticalœ">
           {/* <div className="one">
             <svg fill="transparent" preserveAspectRatio="none" viewBox="0 0 1680 40" className="banner" style={{bottom: "-1px"}}>
-              <path d="M0 40h1680V30S1340 0 840 0 0 30 0 30z" fill="#fff" className='path'></path>
+            <path d="M0 40h1680V30S1340 0 840 0 0 30 0 30z" fill="#fff" className='path'></path>
             </svg>
-          </div>
-
-          <div id="ancre2" className="two center-vertical pg2">
-
+            </div>
+            
+            <div id="ancre2" className="two center-vertical pg2">
+            
             <h1>CE QUE JE PEUX FAIRE</h1>
-
+            
             <span className="underlinexxs"></span>
 
             <div className="competences">
-
-              <div className="competenceLeft">
-                <div className="title">
-                  {"CRÉATION D'APPLICATION WEB"}
-                </div>
-                <div className="description">
-                  {"  De la conception du site web, à son déploiement, seul ou en équipe en passant par le choix des technologies "}
-                </div>
+            
+            <div className="competenceLeft">
+            <div className="title">
+            {"CRÉATION D'APPLICATION WEB"}
+            </div>
+            <div className="description">
+            {"  De la conception du site web, à son déploiement, seul ou en équipe en passant par le choix des technologies "}
+            </div>
               </div>
-
+              
               <div className="competenceRight">
                 <div className="title">
                   {"SITE WEB RESPONSIVE"}
                 </div>
                 <div className="description">
-                  {"  Un site web qui s'adapte à tout type d'interface. Que ce soit sur ordinateur portable, telephone mobile ou tablette."}
+                {"  Un site web qui s'adapte à tout type d'interface. Que ce soit sur ordinateur portable, telephone mobile ou tablette."}
                 </div>
-              </div>
+                </div>
 
-              <div className="competenceLeft">
+                <div className="competenceLeft">
                 <div className="title">
-                  {" INTÉGRATION WEB"}
+                {" INTÉGRATION WEB"}
                 </div>
                 <div className="description">
                   {" Integration HTML/CSS à partir d'une maquette en restant fidèle à cette dernière."}
                 </div>
-              </div>
-
-              <div className="competenceRight">
+                </div>
+                
+                <div className="competenceRight">
                 <div className="title">
-                  {"CONCEPTION GRAPHIQUE"}
+                {"CONCEPTION GRAPHIQUE"}
                 </div>
                 <div className="description">
-                  {"Realisation de logo, flyers, plaquette publicitaire etc ..."}
+                {"Realisation de logo, flyers, plaquette publicitaire etc ..."}
                 </div>
               </div>
 
@@ -102,89 +101,99 @@ export default function Projets() {
           </div>
 
           <div className="three">
-            <div className="container">
+          <div className="container">
               <div className="head">
                 <h3>{"DES SITES WEB RESPONSIVES ADAPTÉS À TOUT TYPE    D'INTERFACE"}</h3>
               </div>
               <Image className="img" alt="img" src="/images/screen.png" layout="fill" />
             </div>
             
-          </div>
-          <div className="competencesT">
-              <h1>{"Mes compétences techniques"}</h1>
-              <div className="competencesTC orientationH ">
-                
-                <div className="items">
-                  <p>HTML</p>
+            </div>
+            <div className="competencesT">
+            <h1>{"Mes compétences techniques"}</h1>
+            <div className="competencesTC orientationH ">
+            
+            <div className="items">
+            <p>HTML</p>
+            <div className="container">
+            <div className="skills html">90%</div>
+            </div>
+            
+            <p>CSS</p>
                   <div className="container">
-                    <div className="skills html">90%</div>
+                  <div className="skills css">90%</div>
                   </div>
-
-                  <p>CSS</p>
-                  <div className="container">
-                    <div className="skills css">90%</div>
-                  </div>
-
+                  
                   <p>JavaScript</p>
                   <div className="container">
-                    <div className="skills js">95%</div>
+                  <div className="skills js">95%</div>
                   </div>
-
+                  
                   <p>PHP</p>
                   <div className="container">
-                    <div className="skills php">60%</div>
+                  <div className="skills php">60%</div>
                   </div>
                   <p>Python</p>
                   <div className="container">
-                    <div className="skills python">60%</div>
+                  <div className="skills python">60%</div>
                   </div>
 
                   <p>C</p>
                   <div className="container">
                     <div className="skills next">90%</div>
-                  </div>
+                    </div>
 
-                  <p>Matlab</p>
+                    <p>Matlab</p>
                   <div className="container">
-                    <div className="skills matlab">90%</div>
+                  <div className="skills matlab">90%</div>
                   </div>
-                </div>
-
-                <div className="items">
+                  </div>
+                  
+                  <div className="items">
                   <p>Next.js</p>
                   <div className="container">
-                    <div className="skills next">80%</div>
+                  <div className="skills next">80%</div>
                   </div>
-
+                  
                   <p>React.js</p>
                   <div className="container">
-                    <div className="skills react">80%</div>
+                  <div className="skills react">80%</div>
                   </div>
-
+                  
                   <p>Node.js</p>
                   <div className="container">
-                    <div className="skills node">85%</div>
+                  <div className="skills node">85%</div>
                   </div>
-
+                  
                   <p>Android Studio</p>
                   <div className="container">
-                    <div className="skills android">75%</div>
+                  <div className="skills android">75%</div>
                   </div>
-
+                  
                   <div className="database">
-                    <div className="mysql"></div>
-                    <div className="mongo"></div>
-                    <div className="postgre"></div>
-                    <div className="aws"></div>
+                  <div className="mysql"></div>
+                  <div className="mongo"></div>
+                  <div className="postgre"></div>
+                  <div className="aws"></div>
                   </div>
-                </div>
-
-              </div>
-          </div> */}
+                  </div>
+                  
+                  </div>
+                </div> */}
 
           <div className="four">
             <div className="portfolio">
-              <h1>MES PROJETS</h1>
+              <div style={{display:"flex",alignItems:"center",justifyContent:"space-between"}}>
+                <div>
+                  <p>{new Date().toDateString()}</p>
+                  <h1>MES PROJETS</h1>
+                </div>
+                <Link href={"#"}>
+                  <a>
+                    <FaHome fontSize={24}/>
+                  </a>
+                </Link>
+              </div>
               <div className="projets">
                 <ProjectCard
                   data = {{
