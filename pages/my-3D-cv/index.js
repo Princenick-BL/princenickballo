0afa2e1755af3ac747cd776a71b1b1986cc2ee76
Princenick-BL <princenickballo@gmail.com>
2022-09-04 15:39:24 +0200

    updates

--- pages/my-3D-cv/index.js
+++ pages/my-3D-cv/index.js
@@ -4,7 +4,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import { Stats, OrbitControls } from "@react-three/drei";
 import { FaHome } from 'react-icons/fa';
-
+import Popup from '../../components/Popup'
 
 import { SmallBox, Wall, Box, Ball, Ground } from "../../components/CVComponents/scene";
 
@@ -38,6 +38,9 @@ function DynamicResolution({ min = 0.1, max = 1, step = 0.1 }) {
 }
 
 const App = () => {
+
+    const [hide,setHide] = useState(true)
+
   return (
     <>
         <Link href={"/"} >
@@ -45,9 +48,13 @@ const App = () => {
                 <FaHome fontSize={24} />
             </a>
         </Link>
+        {hide && (
+            <Popup onClick={(e)=>{setHide(false)}}/>
+        )}
         <Canvas
             style={{ height: "100vh", width: "100vw" }}
-            camera={{ position: [1, 5, 30] }}
+            camera={{ position: [1, 5, 30],isPerspectiveCamera:true }}
+        
         >
             <pointLight position={[15, 15, 15]} />
             <SmallBox />
