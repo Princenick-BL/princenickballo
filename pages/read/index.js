8b291e81cb0959c42e699b5c388d23e3dd0817a1
Princenick-BL <princenickballo@gmail.com>
2022-09-13 01:26:48 +0200

    wip

--- pages/read/index.js
+++ pages/read/index.js
@@ -1,4 +1,4 @@
-import React,{useEffect,useState} from 'react'
+import React,{useEffect,useState,useRef} from 'react'
 import Head from 'next/head'
 import {HomeMenu} from '../../components/Menu'
 import styles from './index.module.scss'
@@ -9,56 +9,130 @@ import Slideshow from '../../components/SlideShow'
 import dynamic from "next/dynamic";
 
 
+const ampStoryPlayerUrl = "https://cdn.ampproject.org/amp-story-player-v0";
+
+function getAmpScript(callback) {
+  const ampJS = document.createElement("script");
+  ampJS.async = true;
+  ampJS.src = ampStoryPlayerUrl + ".js";
+  ampJS.onload = callback;
+  return ampJS;
+}
+
+function getAmpCss() {
+  const ampCSS = document.createElement("link");
+  ampCSS.href = ampStoryPlayerUrl + ".css";
+  ampCSS.rel = "stylesheet";
+  ampCSS.type = "text/css";
+
+  return ampCSS;
+}
+
+const useAmpStoryPlayer = (callback) => {
+  useEffect(() => {
+    console.log("useAmpStoryPlayer");
+    const ampScript = document.querySelector(
+      `script[src="${ampStoryPlayerUrl + ".js"}"]`
+    );
+    if (!ampScript) {
+      document.head.appendChild(getAmpScript(callback));
+      document.head.appendChild(getAmpCss());
+    }
+  }, []);
+};
+
+const playerDo = (containerId, callback) => {
+  const player = document.querySelector(`#player2`);
+  if (player.isReady) {
+    console.log("player is ready");
+    callback(player);
+  } else {
+    console.log("player is not ready");
+    player.addEventListener("ready", () => {
+      callback(player);
+    });
+    
+  }
+  
+};
+
+const loadPlayer = (playerRef) => () => {
+  console.log("player", playerRef.current);
+  if (window.AmpStoryPlayer) {
+    console.log("window");
+    new window.AmpStoryPlayer(window, playerRef.current).load();
+  }
+  const lightboxEl = document.querySelector(".lightbox");
+  
+  playerRef.current.add([
+    {
+      href:
+        "https://stories.marmiton.org/menu-de-la-semaine-4-10-janvier-dE3b4YkgP/"
+    }
+  ]);
+};
 
 function ReadIndex() {
 
   const [mounted,setMounted] = useState(false)
   const player = null
+  const playerRef = useRef(null);
+  useAmpStoryPlayer(loadPlayer(playerRef));
 
   useEffect(()=>{
     setMounted(true)
   },['init'])
 
   useEffect(()=>{
-
     const player = document.getElementById("player2");
-    
-    if(player){
-      const lightboxEl = document.querySelector(".lightbox");
-    
-      if (player.isReady) {
-        initializeCarousel();
-      } else {
-        player.addEventListener("ready", () => {
-          initializeCarousel();
-        });
-      }
-    
-      function initializeCarousel() {
-        const stories = player.getStories();
-    
-        const thumbnails = document.querySelectorAll(
-          ".entry-point-card-container .img"
-        );
-  
-        console.log("OKKK", thumbnails)
-    
-        thumbnails.forEach((img, idx) => {
-          img.addEventListener("click", () => {
-            console.log("CLickk")
-            player.show(stories[idx].href);
-            player.play();
-            lightboxEl.classList.toggle("show");
-          });
-        });
-      }
-    
+    const lightboxEl = document.querySelector(".lightbox");
+    if (player){
       player.addEventListener("amp-story-player-close", () => {
         player.pause();
         lightboxEl.classList.toggle("show");
       });
     }
-  },[mounted])
+  }, [mounted, playerRef])
+
+  // useEffect(()=>{
+
+  //   const player = document.getElementById("player2");
+    
+  //     const lightboxEl = document.querySelector(".lightbox");
+    
+  //     if (player.isReady) {
+  //       initializeCarousel();
+  //     } else {
+  //       player.addEventListener("ready", () => {
+  //         initializeCarousel();
+  //       });
+  //     }
+    
+  //     function initializeCarousel() {
+  //       const stories = player.getStories();
+    
+  //       const thumbnails = document.querySelectorAll(
+  //         ".entry-point-card-container .img"
+  //       );
+  
+  //       console.log("OKKK", thumbnails)
+    
+  //       thumbnails.forEach((img, idx) => {
+  //         img.addEventListener("click", () => {
+  //           console.log("CLickk")
+  //           player.show(stories[idx].href);
+  //           player.play();
+  //           lightboxEl.classList.toggle("show");
+  //         });
+  //       });
+  //     }
+    
+  //     player.addEventListener("amp-story-player-close", () => {
+  //       player.pause();
+  //       lightboxEl.classList.toggle("show");
+  //     });
+  //   }
+  // },[mounted])
 
   return (
     <>
@@ -74,7 +148,7 @@ function ReadIndex() {
         />
       </Head>
       <div>
-        <HomeMenu />
+        {/* <HomeMenu /> */}
         <main className={styles.main}>
           <h3>Main articles</h3>
           <br></br>
@@ -147,7 +221,9 @@ function ReadIndex() {
               </Link>
             </div>
             <div className={styles.mainPlayer}>
-                {/* <amp-story-player layout="fill" width="360" height="600" amp-cache="cdn.ampproject.org">
+              {mounted &&
+              (
+                <amp-story-player layout="fill" width="360" height="600" amp-cache="cdn.ampproject.org">
                   <a href="https://preview.amp.dev/documentation/examples/introduction/stories_in_amp/">
                     <img src="https://amp.dev/static/samples/img/story_dog2_portrait.jpg" width="360" height="600" loading="lazy" data-amp-story-player-poster-img/>
                       Stories in AMP - Hello World
@@ -160,7 +236,9 @@ function ReadIndex() {
                     <img src="https://amp.dev/static/samples/img/story_dog2_portrait.jpg" width="360" height="600" loading="lazy" data-amp-story-player-poster-img />
                     Stories in AMP - Hello World
                   </a>
-                </amp-story-player> */}
+                </amp-story-player>
+
+              )}
             </div>
           </div>
             <div className="viewport">
@@ -169,7 +247,19 @@ function ReadIndex() {
                 <div className="circular-entry-point">
                   <div className="entry-points">
                     <div className="entry-point-card-container">
-                      <Image className='img' width={100} height={100} src="https://picsum.photos/100/100" style={{ borderColor: "#FF6F32"}}/>
+                    <Image className='img' width={100} height={100} src="https://picsum.photos/100/100" style={{ borderColor: "#FF6F32" }} 
+                      onClick={(e)=>{playerDo("the-id", (player) => {
+                        console.log("show", player);
+
+                        player.show(
+                          "https://wsdemos.uc.r.appspot.com/ampfest/s1"
+                        );
+                        player.play();
+                        player.unmute();
+                      });
+                        const lightboxEl = document.querySelector(".lightbox");
+                        lightboxEl.classList.toggle("show");
+                      }}/>
                       <div>
                         <span className="entry-point-card-title">Q&A with ZOE Newman</span>
                       </div>
@@ -199,7 +289,8 @@ function ReadIndex() {
 
               <div className="lightbox">
               {mounted &&
-              <amp-story-player className="my-player" id="player2">
+                <amp-story-player className="my-player" id="player2" ref={playerRef}
+>
                   <script type="application/json">
                     {`{
                       "behavior": {
