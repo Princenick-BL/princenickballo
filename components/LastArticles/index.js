9cf76cfbc6a664a466f34f94f8f59269606305e6
PrinceNick-BL <princenickballo@gmail.com>
2022-09-17 16:19:10 +0200

    wip

--- components/LastArticles/index.js
+++ components/LastArticles/index.js
@@ -1,7 +1,53 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import styles from './index.module.scss'
+import Image from 'next/image'
+
+
+const Card = () =>{
+    return(
+        <div className={styles.card} >
+            <div className={styles.publisher}>
+                <Image
+                    src={"https://picsum.photos/500/400"}
+                    width="40"
+                    height="40"
+                    layout="fixed"
+                    className={styles.publisher_logo}
+                />
+                <div className={styles.name}>Published by <strong>Prince Nick Ballo</strong> on Sep,14,2022</div>
+            </div>
+            <br></br>
+            <div className={styles.title}>
+                <h1>
+                    Pourquoi choisir react js au lieu de next js et vis versa
+                </h1>
+                <span>{"ReactJS - Web - Web3 - Javascript"}</span>
+            </div>
+            <div className={styles.read}>150 read</div>
+        </div>
+    )
+}
 
 export default function LasrArticle() {
+
+    const [data,setData] = useState([
+        {
+            id:1,
+        },
+        {
+            id:1,
+        },
+        {
+            id:1,
+        },
+        {
+            id:1,
+        },
+        {
+            id:1,
+        }
+    ])
+
   return (
     <div className={styles.wrapper}>
 
@@ -11,7 +57,6 @@ export default function LasrArticle() {
             height: "max-content",
             overflowY: "hidden",
             paddingTop: "3rem",
-            paddingLeft: "1rem",
             paddingBottom: ".5rem"
         }}>
             <div className={styles.mostreadedArticle}>
@@ -25,18 +70,12 @@ export default function LasrArticle() {
                     </div>
                 </div>
             </div>
-            <div className={styles.card} >
-            
-            </div>
-            <div className={styles.card} >
-            
-            </div>
-            <div className={styles.card} >
-            
-            </div>
-            <div className={styles.card} >
-            
-            </div>
+            {data.map((card=>{
+                return(
+                    <Card/>
+                )
+            }))}
+           
         </div>
     </div>
   )
