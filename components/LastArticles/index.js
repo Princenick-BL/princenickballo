cdfa62066564063c8eb1da23d3b638241b630b24
PrinceNick-BL <princenickballo@gmail.com>
2022-09-17 15:35:47 +0200

    wip

--- components/LastArticles/index.js
+++ components/LastArticles/index.js
@@ -5,15 +5,20 @@ export default function LasrArticle() {
   return (
     <div className={styles.wrapper}>
 
-            <div classNamz={styles.container} style={{
-                display:"flex",
-                width: "100%",
-                height: "max-content",
-                overflowY: "hidden",
-                paddingTop: "3rem",
-                paddingLeft: "1rem",
-                paddingBottom: ".5rem"
-                }}>
+        <div classNamz={styles.container} style={{
+            display:"flex",
+            width: "100%",
+            height: "max-content",
+            overflowY: "hidden",
+            paddingTop: "3rem",
+            paddingLeft: "1rem",
+            paddingBottom: ".5rem"
+        }}>
+            <div className={styles.mostreadedArticle}>
+                <div className={styles.title}>
+                    Most readed article
+                </div>
+            </div>
             <div className={styles.card} >
             
             </div>
