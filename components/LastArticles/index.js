7002da9e6ff6cdd1a3061709f8f94986c6da8855
PrinceNick-BL <princenickballo@gmail.com>
2022-09-17 15:08:11 +0200

    wip

--- components/LastArticles/index.js
+++ components/LastArticles/index.js
@@ -11,56 +11,20 @@ export default function LasrArticle() {
                 height: "max-content",
                 overflowY: "hidden",
                 paddingTop: "3rem",
-                paddingLeft: "3rem",
-                paddingBottom: "3rem"
+                paddingLeft: "1rem",
+                paddingBottom: ".5rem"
                 }}>
             <div className={styles.card} >
-            <h3 className={styles.title} >Card 1</h3>
-            <div className={styles.bar} >
-                <div className={styles.emptybar} ></div>
-                <div className={styles.filledbar} ></div>
-            </div>
-            <div className={styles.circle} >
-                <svg version="1.1" >
-                <circle className={styles.stroke} cx="60" cy="60" r="50"/>
-            </svg>
-            </div>
+            
             </div>
             <div className={styles.card} >
-            <h3 className={styles.title} >Card 2</h3>
-            <div className={styles.bar} >
-                <div className={styles.emptybar} ></div>
-                <div className={styles.filledbar} ></div>
-            </div>
-            <div className={styles.circle} >
-                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" >
-                <circle className={styles.stroke} cx="60" cy="60" r="50"/>
-            </svg>
-            </div>
+            
             </div>
             <div className={styles.card} >
-            <h3 className={styles.title} >Card 3</h3>
-            <div className={styles.bar} >
-                <div className={styles.emptybar} ></div>
-                <div className={styles.filledbar} ></div>
-            </div>
-            <div className={styles.circle} >
-                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" >
-                <circle className={styles.stroke} cx="60" cy="60" r="50"/>
-            </svg>
-            </div>
+            
             </div>
             <div className={styles.card} >
-            <h3 className={styles.title} >Card 4</h3>
-            <div className={styles.bar} >
-                <div className={styles.emptybar} ></div>
-                <div className={styles.filledbar} ></div>
-            </div>
-            <div className={styles.circle} >
-                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" >
-                <circle className={styles.stroke} cx="60" cy="60" r="50"/>
-            </svg>
-            </div>
+            
             </div>
         </div>
     </div>
