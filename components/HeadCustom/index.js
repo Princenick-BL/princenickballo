8989eb337422dbb023188a8d5f818b1297d4246a
PrinceNickBALLO <prince@zmooz.com>
2023-06-20 20:57:28 +0200

    wip

--- components/HeadCustom/index.js
+++ components/HeadCustom/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
 
-export default function HeadCustom({theme="#fff",preview = {preview}}) {
+export default function HeadCustom({theme="#fff",preview = "/images/preview.png"}) {
   return (
     <Head>
     {/* <!-- HTML Meta Tags --> */}
